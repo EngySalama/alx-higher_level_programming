4c57e0565c5d94a0fd2ab5c1ca1ee2181c111e18
EngySalam <engysalama1992@gmail.com>
2023-11-28 18:36:24 -0800

    a

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,31 +1,49 @@
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * insert_node -  inserts a number into a sorted singly linked list.
- * @head: the head node of the list.
- * @number: the value of the new node.
+ * insert_node - function that inserts a number to a singly linked list.
+ * @head: pointer to pointer to head of the list.
+ * @number: value of the node to insert.
  *
- * Return: the new node or null.
+ * Return: the address of the new node, or NULL if it failed.
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node, *new;
-
-	node = *head;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	listint_t *new_node, *traverse;
+	unsigned int idx = 0, i = 0;
+	/* if there is no list return null */
+	if (head == NULL)
 		return (NULL);
-	new->n = number;
-	if (node == NULL || node->n >= number)
+	/* create the new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	/* access the n field of the new_node and initialize it as n */
+	new_node->n = number;
+	/* check if idx = 0 */
+	if (idx == 0)
 	{
-		new->next = node;
-		*head = new;
-		return (new);
+		/* access the next field of new_node and assign it as first node */
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	while (node && node->next && node->next->n < number)
-		node = node->next;
-	new->next = node->next;
-	node->next = new;
-	return (new);
+	/* make traverse be the value at head */
+	traverse = *head;
+	while (i != idx - 1 && traverse != NULL)
+	{
+		traverse = traverse->next;
+		i++;
+	}
+
+	if (i == idx - 1 && traverse != NULL)
+	{
+		new_node->next = traverse->next;
+		traverse->next = new_node;
+		return (new_node);
+	}
+	return (NULL);
 }
