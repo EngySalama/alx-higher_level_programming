99241a30b49d53de376a20ec731aab4a86c48ff8
EngySalama <engysalama1992@gmail.com>
2024-01-09 15:35:46 -0800

    a

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Function From JSON string to Object"""
+import json
 
 
-def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+def from_json_string(my_str):
+    """Function that returns an object (Python data structure)
+    represented by a JSON string"""
+
+    return json.loads(my_str)
