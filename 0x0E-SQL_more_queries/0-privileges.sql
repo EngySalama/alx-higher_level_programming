e5e50a7d61c376f6adce67022d65bbb1fa822e01
EngySalama <engysalama1992@gmail.com>
2024-02-14 14:42:52 -0800

    a

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,1 +1,3 @@
-MySQL script that lists all privileges of the users user_0d_1 and user_0d_2.
+-- List all privileges for users 'user_0d_1' and 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
